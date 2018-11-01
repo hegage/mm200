d2bf7b9676a5fcc90d05e4d77c73b2cbe407c64d
Hege Aanjesen <hege.aanj@gmail.com>
2018-11-01 21:39:02 +0100

    bruk lokal database lokalt

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -1,11 +1,23 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host     : process.env.JAWSDB_URL,
-    user     : process.env.JAWSDB_US,
-    password : process.env.JAWSDB_PW,
-    database : process.env.JAWSDB_DB
-});
-connection.connect();
+if(typeof process.env.prod === 'undefined') {
+    
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'password',
+        database : 'todoapp'
+    });
+    
+} else {
+   
+    var connection = mysql.createConnection({
+        host     : process.env.JAWSDB_URL,
+        user     : process.env.JAWSDB_US,
+        password : process.env.JAWSDB_PW,
+        database : process.env.JAWSDB_DB
+    });
+}
+    connection.connect();
 
 module.exports = connection;
