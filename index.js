a4d82f1e73f215c44f1477033bb2b8459f1c4b17
Hege Aanjesen <hege.aanj@gmail.com>
2018-11-01 21:38:17 +0100

    cors

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 4200;
 
 app.use(bodyParser.json())
-
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 routes(app);
 
 app.listen(port);
