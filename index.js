4fc0868c01a6044615c1d8ae947d4adc3012a19c
Hege Aanjesen <hege.aanj@gmail.com>
2018-11-22 20:26:15 +0100

    Trying the old cors

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const cors       = require('cors');
 const routes     = require('./routes/routes');
 const jwt        = require('express-jwt');
 const port       = process.env.PORT || 4200;
+
 const app        = express();
 app.use([cors(), jwt({ secret: process.env.SECRET || 'UniCornsCannaeFly1337'}).unless({path: ['/login', '/validate']})])
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://todoappaanj-frontend.herokuapp.com");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+
+    next();
+});
 app.use(bodyparser.json())
 routes(app);
 
