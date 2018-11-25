8830c7d3b88156473d7d9a0b048117999fc66a59
Hege Aanjesen <hege.aanj@gmail.com>
2018-11-25 17:39:07 +0100

    Change localhost url

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 
 if(typeof process.env.prod === 'undefined') {
-    
+
     var connection = mysql.createConnection({
-        host     : 'localhost',
+        host     : '172.17.0.2',
         user     : 'root',
         password : 'password',
         database : 'todoapp'
     });
-    
+
 } else {
-   
+
     var connection = mysql.createConnection({
         host     : process.env.JAWSDB_URL,
         user     : process.env.JAWSDB_US,
@@ -18,6 +18,6 @@ if(typeof process.env.prod === 'undefined') {
         database : process.env.JAWSDB_DB
     });
 }
-    connection.connect();
+connection.connect();
 
 module.exports = connection;
